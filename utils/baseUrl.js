206fd385a285404c18a014074b4f64c690b85870
codenaij <codenaij@gmail.com>
2022-09-29 05:42:55 +0100

    changed url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3000'
-    : 'https://def-project.herokuapp.com'
+    : 'https://def-project-a9d9-1bc52q7ri-codenaij.vercel.app'
+// : 'https://def-project.herokuapp.com'
 
 module.exports = baseUrl
